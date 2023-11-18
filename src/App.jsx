78a55430efb97dc5e8ca014fa40a38966e8f3399
archanamech1660 <archanamech16@gmail.com>
2023-11-18 13:52:48 +0530

    initial-setup:remove subscribe page

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { ProductsDashboardPage } from './components/pages/ProductsDashboardPage'
 import { ProductPage } from './components/pages/ProductPage';
 import { CartPage } from './components/pages/CartPage';
 import { RootLayout } from './components/templates/RootLayout';
-import { SubscribePage } from './components/pages/SubscribePage';
 import { Provider } from "react-redux"
 import store from "./store/store";
 import LoginPage from './components/pages/LoginPage';
@@ -17,7 +16,6 @@ function App() {
         {/*Dashboard is a default component */}
         <Route index element={<ProductsDashboardPage />}></Route>
         <Route path='/cart' element={<CartPage />}></Route>
-        <Route path='/subscribe' element={<SubscribePage />}></Route>
         <Route path='/product/:id' element={<ProductPage />}></Route>
         <Route path='/electronics' element={<ProductsDashboardPage category="electronics" />}></Route>
         <Route path='/jewelery' element={<ProductsDashboardPage category="jewelery" />}></Route>
--- src/components/organisms/NavBarPanel.jsx
+++ src/components/organisms/NavBarPanel.jsx
@@ -17,7 +17,6 @@ export const NavBarPanel = () => {
         <ul className="flex flex-col space-y-3 md:flex-row md:space-y-0 md:space-x-3 ">
             <li className="group"><Link to="/">Dashboard</Link><div className="border-black border-t-2 opacity-0 group-hover:opacity-100 duration-200"></div></li>
             <li className="group"><Link to="/cart">Cart</Link><div className="border-black border-t-2 opacity-0 group-hover:opacity-100 duration-200"></div></li>
-            <li className="group"><Link to="/subscribe">Subscribe</Link><div className="border-black border-t-2 opacity-0 group-hover:opacity-100 duration-200"></div></li>
         </ul>
         <div className="flex space-x-6">
             <SearchInput />
