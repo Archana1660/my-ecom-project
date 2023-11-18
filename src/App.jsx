4439dae93b9513ffebf029e4c5aec704b69ad503
archanamech1660 <archanamech16@gmail.com>
2023-11-18 10:44:39 +0530

    error-page:add error page

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,13 @@ import { SubscribePage } from './components/pages/SubscribePage';
 import { Provider } from "react-redux"
 import store from "./store/store";
 import LoginPage from './components/pages/LoginPage';
+import { ErrorPage } from './components/pages/ErrorPage/ErrorPage';
 
 function App() {
   const router = createBrowserRouter(createRoutesFromElements(
     <>
       <Route path="login" element={<LoginPage />} />
-      <Route path="/" element={<RootLayout />}>
+      <Route path="/" element={<RootLayout />} errorElement={<ErrorPage/>}>
         {/*Dashboard is a default component */}
         <Route index element={<ProductsDashboardPage />}></Route>
         <Route path='/cart' element={<CartPage />}></Route>
