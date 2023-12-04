accf33d574a4e5ff115acef42b3fcbc6b8a2502e
archanamech1660 <archanamech16@gmail.com>
2023-12-04 15:15:34 +0530

    product-navbar:add navigation with burger menu for small screen

--- src/components/atoms/SearchInput.jsx
+++ src/components/atoms/SearchInput.jsx
@@ -1,8 +1,10 @@
+import { WorkInProgress } from '../molecules/WorkInProgress';
 import searchIcon from '../../assets/searchIcon.png';
 
 export const SearchInput = () => {
-    return <div className='border-b flex items-center'>
-        <input type="text" className=" px-3 py-2 rounded-lg focus:outline-none" placeholder="Search" />
-        <button><img src={searchIcon} alt="searchIcon" className='w-4 h-4' /></button>
+    return <div className='border-b flex items-center group relative'>
+        <input type="text" className=" px-3 py-2 rounded-lg focus:outline-none" placeholder="Search" disabled/>
+        <button disabled className='hidden xs:inline-block'><img src={searchIcon} alt="searchIcon" className='w-4 h-4' /></button>
+        <WorkInProgress/>
     </div>
 }
--- src/components/organisms/NavBarPanel.jsx
+++ src/components/organisms/NavBarPanel.jsx
@@ -1,15 +1,20 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { NavLink, useLocation } from "react-router-dom"
 import { useDispatch, useSelector } from "react-redux";
+
 import { SearchInput } from "../atoms/SearchInput"
 import { CartCount } from "../molecules/CartCount"
 import { getAllProductCategories } from "../../store/ProductDashboardStore/ProductCategoriesSlice"
 import './NavBarPanel.css';
-
+import { burgerMenuOpen, burgerMenuClose } from "../../assets";
+    
 export const NavBarPanel = () => {
     const dispatch = useDispatch()
-    const { data: allCategories } = useSelector(state => state.ALL_CATEGORIES)
     let location = useLocation();
+
+    const [showBurgerMenu, setShowBurgerMenu] = useState(false)
+
+    const { data: allCategories } = useSelector(state => state.ALL_CATEGORIES)
 
     useEffect(() => {
         dispatch(getAllProductCategories())
@@ -17,35 +22,50 @@ export const NavBarPanel = () => {
 
     const handleActiveLink = ({isActive}) => {
         if (isActive) {
-        return {borderBottom: '2px solid' }
-            } 
+            return {borderBottom: '2px solid' }
+        } 
+    }
+
+    const handleShowMenu = () => {
+        setShowBurgerMenu(!showBurgerMenu)
     }
 
     return (
         <div className="bg-rose-50 rounded-lg  shadow-lg">
             <nav className="flex items-center p-4 m-2 justify-between border-b board-grap-500">
-                <ul className="flex flex-col space-y-3 md:flex-row md:space-y-0 md:space-x-3 ">
+                <ul className="hidden md:flex flex-col space-y-3 md:flex-row md:space-y-0 md:space-x-3 ">
                     <li className="group">
-                        <NavLink className="border-black group-hover:border-b-2 duration-200" style={handleActiveLink} to="/">All Categories</NavLink>
+                        <NavLink className="border-black group-hover:border-b-2 duration-200" style={handleActiveLink} to="/">Categories</NavLink>
                     </li>
                     <li className="group">
                         <NavLink className="border-black group-hover:border-b-2 duration-200" style={handleActiveLink} to="/cart">Cart</NavLink>
                     </li>
                 </ul>
+                <nav className="main-navigation md:hidden block relative">
+                    <img src={showBurgerMenu ? burgerMenuClose : burgerMenuOpen} alt={showBurgerMenu ? burgerMenuOpen : burgerMenuClose} className="w-8 h-8" onClick={handleShowMenu} />
+                    <ul className={`p-5 rounded-xl shadow-2xl bg-white z-10 duration-200 absolute ${showBurgerMenu ? 'opacity-100 block': 'opacity-0 hidden'}`}>
+                       <li className="group">
+                        <NavLink className="border-black group-hover:border-b-2 duration-200" style={handleActiveLink} to="/">Categories</NavLink>
+                    </li>
+                    <li className="group">
+                        <NavLink className="border-black group-hover:border-b-2 duration-200" style={handleActiveLink} to="/cart">Cart</NavLink>
+                    </li>
+                    </ul>
+                </nav> 
                 <div className="flex space-x-6">
                     <SearchInput />
                     <CartCount />
                 </div>
             </nav>
         
-            {(location?.pathname !== '/cart')? <nav className="p-4 m-2">
-                <ul className="flex justify-around">
+            {(location?.pathname !== '/cart')? <nav className="category-list p-4 m-2">
+                <ul className="flex flex-wrap justify-around">
                     {allCategories?.map(category => {
-                        return <li key={category}><NavLink to={`${category}`}>{category}</NavLink></li>
+                        return <li key={category} className="group"><NavLink className="px-1 my-1 group-hover:border-b-2" to={`${category}`}>{category}</NavLink></li>
                     })}
                 </ul>
-            </nav>: null}
-           
+            </nav> : null
+            }
         </div>
     )
 }
