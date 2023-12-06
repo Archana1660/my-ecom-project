3dd7a77aa9e83cb8b1d3fe8c64bdfde0c0a0b069
archanamech1660 <archanamech16@gmail.com>
2023-12-06 23:14:44 +0530

    product-page: add empty cart message.

--- src/components/pages/CartPage.jsx
+++ src/components/pages/CartPage.jsx
@@ -5,17 +5,17 @@ export const CartPage = () => {
     const products = useSelector(state => state.CART)
     return <main>
         <h1 className="font-bold text-2xl">Cart Dashboard</h1>
-           <section className="grid grid-cols-4 gap-4">
+           
 
         {products.length > 0 ? 
-                <div>
+                <section className="grid grid-cols-4 gap-4">
                     {products.map((product) => {
                         return <Card key={product.id} itemDetail={product} showRemoveButton={true} />
                     })}
-                </div>
+                     </section>
+
                 :
-                <p>Your Cart is empty! Please continue shopping</p>
+                <p>Your Cart is empty! Please continue shopping.</p>
         }
-     </section>
     </main>
 }
