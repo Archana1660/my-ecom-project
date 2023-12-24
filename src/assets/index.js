1db28bf81fae60b25c06b737399d914c6fc9e025
archanamech1660 <archanamech16@gmail.com>
2023-12-25 02:41:13 +0530

    main:rearranging the images to fix deploy issue.

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,13 +4,12 @@ import errorImg from './embarrassed.png'
 import homePageIcon from './home.gif'
 
 //product images
-import camera from './productImages/electronics/camera.jpg'
-import soundCheck from './productImages/electronics/soundCheck.jpg'
+import soundCheck from './soundCheck.jpg'
 
-import ring_braclet_earring_set from './productImages/jewellery/ring_braclet_earring_set.jpg'
+import ring_braclet_earring_set from './ring_braclet_earring_set.jpg'
 
-import summar from './productImages/womens_clothing/summar.jpg'
-import mens_jeans_shoe from './productImages/mens_clothing/mens_jeans_shoe.jpg'
+import summar from './summar.jpg'
+import mens_jeans_shoe from './mens_jeans_shoe.jpg'
 
 
-export { burgerMenuOpen, burgerMenuClose, errorImg, homePageIcon, camera, soundCheck, ring_braclet_earring_set, summar, mens_jeans_shoe }
+export { burgerMenuOpen, burgerMenuClose, errorImg, homePageIcon, soundCheck, ring_braclet_earring_set, summar, mens_jeans_shoe }
