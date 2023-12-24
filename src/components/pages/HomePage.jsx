4f0990b12d23df33f06aca2a7365d0ed83af7c99
archanamech1660 <archanamech16@gmail.com>
2023-12-24 19:22:00 +0530

    home-page: add responsiveness.

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -3,24 +3,24 @@ import { Link } from 'react-router-dom'
 export const HomePage = () => {
 
 return (
-    <main className='grid grid-cols-1 grid-rows-1 md:grid-cols-2 d:grid-rows-2 gap-4'>
-    <section className=' h-96 relative group'>
-      <div className='backdrop-blur-sm bg-white/30 w-full h-full absolute hidden group-hover:flex items-center justify-center duration-200'>
+    <main className='grid grid-cols-1 grid-rows-1 lg:grid-cols-2 d:grid-rows-2 gap-4'>
+    <section className='h-auto md:h-96 relative group'>
+      <div className='backdrop-blur-sm bg-white/30 w-full h-full inset-y-2.5 absolute hidden group-hover:flex items-center justify-center duration-200'>
         <p className='text-2xl'>Electronics</p>
       </div>
-        <img src={`${soundCheck}`} alt={`${soundCheck}`} className='w-full h-auto' />
+        <img src={`${soundCheck}`} alt={`${soundCheck}`} className='w-full h-full object-contain' />
     </section>
-    <section className='h-96 flex items-center'>
+    <section className='h-auto md:h-96 flex flex-col md:flex-row items-center justify-center gap-4'>
       <div className='relative group'>
-        <div className='backdrop-blur-sm bg-white/30 w-full h-full absolute hidden group-hover:flex items-center justify-center duration-200'>
-          <p className='text-2xl '>Jwellery</p>
+        <div className='backdrop-blur-sm bg-white/30 w-full h-full inset-y-2.5 absolute hidden group-hover:flex items-center justify-center duration-200'>
+          <p className='text-2xl'>Jewellery</p>
         </div>
         <Link>
               <img src={`${ring_braclet_earring_set}`} alt={`${ring_braclet_earring_set}`} className='duration-200 w-56 h-fit inline-block' />
         </Link>
       </div>
       <div className='relative group'>
-        <div className='backdrop-blur-sm bg-white/30 w-full h-full absolute hidden group-hover:flex items-center justify-center duration-200'>
+        <div className='backdrop-blur-sm bg-white/30 w-full h-full inset-y-2.5 absolute hidden group-hover:flex items-center justify-center duration-200'>
           <p className='text-2xl'>Women&apos;s Clothing</p>
         </div>
         <Link>
@@ -28,7 +28,7 @@ return (
         </Link>
       </div>
       <div className='relative group'>
-        <div className='backdrop-blur-sm bg-white/30 w-full h-full absolute hidden group-hover:flex items-center justify-center duration-200'>
+        <div className='backdrop-blur-sm bg-white/30 w-full h-full inset-y-2.5 absolute hidden group-hover:flex items-center justify-center duration-200'>
            <p className='text-2xl'>Men&apos;s Clothing</p>
         </div>
         <Link>
