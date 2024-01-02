d5491d0fff24e71d66047b7ad9a61e12ecfa8236
archanamech1660 <archanamech16@gmail.com>
2024-01-02 17:46:03 +0530

    main:corrected the path for images

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,12 +4,12 @@ import errorImg from './embarrassed.png'
 import homePageIcon from './home.gif'
 
 //product images
-import soundCheck from './soundCheck.jpg'
+import soundCheck from './productImage/soundCheck.jpg'
 
-import ring_braclet_earring_set from './ring_braclet_earring_set.jpg'
+import ring_braclet_earring_set from './productImage/ring_braclet_earring_set.jpg'
 
-import summar from './summar.jpg'
-import mens_jeans_shoe from './mens_jeans_shoe.jpg'
+import summar from './productImage/summar.jpg'
+import mens_jeans_shoe from './productImage/mens_jeans_shoe.jpg'
 
 
 export { burgerMenuOpen, burgerMenuClose, errorImg, homePageIcon, soundCheck, ring_braclet_earring_set, summar, mens_jeans_shoe }
