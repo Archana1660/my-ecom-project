91554869e3a07161d3b6f20e8333faca1e241a45
archanamech1660 <archanamech16@gmail.com>
2024-01-03 13:40:26 +0530

    product-dashboard-page:add responsive in skeleton loading

--- src/components/pages/ProductDashboardPage/ProductDashboardSkeleton.jsx
+++ src/components/pages/ProductDashboardPage/ProductDashboardSkeleton.jsx
@@ -3,22 +3,22 @@ export const ProductDashboardSkeleton = () => {
     <div className="flex justify-center min-h-screen ">
         <section className="bg-neutral-100 p-2 m-2 shadow-2xl rounded-3xl md:p-10">
             <div className="grid gap-5 grid-col-1 sm:grid-cols-2 lg:grid-cols-3">
-               <div className="shadow-lg rounded-xl p-10 md:p-5 lg:p-10 bg-white group flex flex-col justify-center w-96">
-                    <div className="bg-gray-300 animate-pulse" style={{height:'15rem', borderRadius:'2rem'}}></div>
+               <div className="shadow-lg rounded-xl p-10 md:p-5 lg:p-10 bg-white group flex flex-col justify-center sm:w-72 lg:w-[22rem]">
+                    <div className="bg-gray-300 animate-pulse rounded-2xl h-56 w-52 self-center"></div>
                       <div className="card-details flex flex-col items-center justify-center space-y-2 mt-5">
                         <div className="h-6 w-full bg-gray-300 rounded-full mb-4"></div>
                         <div className="h-6 w-full bg-gray-300 rounded-full"></div>
                     </div>
                 </div>
-               <div className="shadow-lg rounded-xl p-10 md:p-5 lg:p-10 bg-white group flex flex-col justify-center  w-96">
-                    <div className="bg-gray-300 animate-pulse" style={{height:'15rem', borderRadius:'2rem'}}></div>
+               <div className="shadow-lg rounded-xl p-10 md:p-5 lg:p-10 bg-white group flex flex-col justify-center sm:w-72 lg:w-[22rem]">
+                    <div className="bg-gray-300 animate-pulse rounded-2xl h-56 w-52 self-center"></div>
                       <div className="card-details flex flex-col items-center justify-center space-y-2 mt-5">
                        <div className="h-6 w-full bg-gray-300 rounded-full mb-4"></div>
                         <div className="h-6 w-full bg-gray-300 rounded-full"></div>
                     </div>
                 </div>
-               <div className="shadow-lg rounded-xl p-10 md:p-5 lg:p-10 bg-white group flex flex-col justify-center w-96">
-                    <div className="bg-gray-300 animate-pulse" style={{height:'15rem', borderRadius:'2rem'}}></div>
+               <div className="shadow-lg rounded-xl p-10 md:p-5 lg:p-10 bg-white group flex flex-col justify-center sm:w-72 lg:w-[22rem]">
+                    <div className="bg-gray-300 animate-pulse rounded-2xl h-56 w-52 self-center"></div>
                       <div className="card-details flex flex-col items-center justify-center space-y-2 mt-5">
                        <div className="h-6 w-full bg-gray-300 rounded-full mb-4"></div>
                         <div className="h-6 w-full bg-gray-300 rounded-full"></div>
